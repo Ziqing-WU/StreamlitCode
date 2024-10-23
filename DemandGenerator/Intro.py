fb2e4a5d8bdc193bb28e716913e6041b13a452d2
zwu@I7029 <ziqingwu@outlook.com>
2024-10-23 10:52:03 +0200

    total market finished

---
+++ DemandGenerator/Intro.py
@@ -0,0 +1,105 @@
+import streamlit as st
+import graphviz
+
+with st.sidebar:
+    """
+    Navigation
+
+    [Demand Estimation](#demand-estimation)
+    - [Demand Estimation Framework](#demand-estimation-framework)
+    - [Application on Electric Retrofit of ICE Vehicle](#application-on-electric-retrofit-of-ice-vehicle)
+    """
+
+
+st.markdown(
+"""
+# Demand Estimation 
+Demand estimation model provides scenarios of demands for new circular service on each product model, at each city and during each period, serves as the input of Decision Support System for circular supply chain network design.
+The outline of the whole PhD project is shown as below. The demand estimation model is one of the three main components presented here.
+"""
+)
+
+st.image('BigPicture.png', width=800, caption='Overview of Project')
+
+st.markdown(
+    """
+    ## Demand Estimation Framework
+    Here is the proposed framework that will be used to estimate demand, which is composed of sets and filters.
+    
+    """
+)
+
+st.image('PoupeeRusse.png', width=800, caption='Overview of Demand Estimation Framework')
+
+"""
+## Application on Electric Retrofit of ICE Vehicle
+"""
+col1, col2 = st.columns([1,1])
+
+with col1:
+    
+    """
+    The analysis pipeline in the demand estimation section is illustrated here: 
+    """
+    pro = graphviz.Digraph()
+    pro.attr('node', shape='box')
+    pro.node('M1', label='Total Market')
+    pro.node('M2', label="Total Addressable Market")
+    pro.node('M3', label='Serviceable Available Market')
+    pro.node('M4', label='Market Share for Similar Service')
+    pro.node('M5', label='Market Share for the Company')
+
+    pro.edge('M1', 'M2', label='Filter 1: Legislative and Technical Constraints')
+    pro.edge('M2', 'M3', label='Filter 2: Strategies and Business Model of the Company')
+    pro.edge('M3', 'M4', label='Filter 3: Consumer Purchasing Intention Analysis')
+    pro.edge('M4', 'M5', label='Filter 4: Competitiveness of the Company')
+
+    st.graphviz_chart(pro)
+
+with col2:
+    st.markdown(
+        """
+        The framework is applied to the case of electric retrofitting of internal combustion engine (ICE) vehicles, inspired by a start-up where I spent 8 months observing their operations. The filters are defined as follows:
+        - Filter 1: Legislative and Technical Constraints
+            - Legislative Constraints
+                - Vehicle Category: L, M, N, not registered as collection vehicle
+                - Vehicle Age: more than 3 years for category L, more than 5 years for category M, N
+                - Vehicle Engine: at least one spark-ignition or compression engine
+                - Geographical Coverage: registered in France
+            - Technical Constraints
+                - Vehicle Type : M1, N1
+                - Vehicle Engine: diesel / petrol
+                - Engine Power: between 60 kW and 110 kW
+                - and other constraints on weights, noise, CO2 emission, etc
+        - Filter 2: Strategies and Business Model of the Company
+            - Geographical Coverage: urban area
+            - Vehicle Model Selection: last vehicle of the model is fabricated at least 5 years ago / models with most vehicles
+            - Consumer Type: toB / toC
+        - Filter 3: Consumer Purchasing Intention Analysis
+            - Executive Factors Identification (click the following expander for an example)
+        - Filter 4: Competitiveness of the Company
+    
+        *N.B.*: This information is used to set default values. Users are welcome to change these filters based on their needs.
+        """
+    )
+with st.expander('Executive Factors for Consumers'):
+
+    st.markdown(
+        """
+    |            | From existing product (P)                                                                                                                                           | From consumer (**C**)                  | From economical, physical and technological environment (**EPTE**)                                                                                                |
+    |-------------|------------------------------------------------------------------------------------------------------------------------------------------------------------------|---------------------------------|---------------------------------------------------------------------------------------------------------------------------------------------------------|
+    | Demography (**D**)  |                                                                                                                                                                 | GDP/capita (**D-C-1**)                     | Population density (**D-EPTE-1**); number of households with internet access (**D-EPTE-2**)                                                                                          |
+    | Environment (**Env**)| Well-to-wheel GHG emission reduction comparing to alternatives (**Env-P-1**); average temperature in summer (lifespan damage) (**Env-P-2**); average temperature in winter (range reduction) (**Env-P-3**) | Attitude towards climate change (**Env-C-1**) | ZFE-m (**Env-EPTE-1**)                                                                                                                                                  |
+    | Economy (**E**)    | Acquisition fee reduction comparing to alternatives (**E-P-1**); garage accessibility (**E-P-2**)                                                                                       | Household disposable income (**E-C-1**)     | Charging point accessibility (**E-EPTE-1**); access to e-lanes (**E-EPTE-2**); fuel cost saving vs gasoline (**E-EPTE-3**); fuel cost saving vs diesel (**E-EPTE-4**); financial incentives (**E-EPTE-5**); existing demo-projects (**E-EPTE-6**) |
+    | Energy (**Ener**)     |                                                                                                                                                                |                                | Share of sustainable electricity in net electricity generation (**Ener-EPTE-1**)                                                                                          |
+    | Transport (**T**)  | Commuting behavior (considering the limits of range) (**T-P-1**)                                                                                                            |                                | Car density (cars/1000 habitants) (**T-EPTE-1**)                                                                                                                      |
+    
+    inspired from [Zubaryeva et al., 2012](https://doi.org/10.1016/j.techfore.2012.06.004)
+        """
+    )
+"""
+Get ready to explore the exciting world of demand estimation for circular businesses—click the button here and join the journey!
+"""
+
+if st.button("Start with the Total Market!"):
+    st.switch_page("TotalMarket.py")
---
+++ DemandGenerator/TotalMarket.py
@@ -0,0 +1,213 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import glob
+import os
+import streamlit as st
+import geopandas as gpd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+from datetime import date
+import csv
+import sys
+
+@st.cache_data
+def create_map():
+    # This file comes from the geoservices product Admin Express COG 2022
+    fp = r'C:\Users\zwu\Documents\Data\CommuneShape\COMMUNE_OCCITANIE.shp'
+    map_df = gpd.read_file(fp)
+    # map_df.to_crs(pyproj.CRS.from_epsg(4326), inplace=True)
+    return map_df[['INSEE_COM', 'geometry']].set_index('INSEE_COM')
+
+# r';(?!\s)'
+
+@st.cache_data
+def load_vehicle_data():
+    fp_vehicle = r'C:\Users\zwu\Documents\Data\Vehicle\occitanie_cleaned_code_INSEE.csv'
+    df = pd.read_csv(fp_vehicle, low_memory=False, dtype=object)
+    return df
+
+st.markdown(
+    """
+    # Total Market
+    """
+)
+
+csv_file = st.file_uploader(
+    'Upload a file of the existing product information'
+    )
+
+st.markdown(
+    """
+    To show case the use of demand estimation framework, here a dataset on ICE vehicles has been loaded.
+    """
+)
+
+if not csv_file:
+    df = load_vehicle_data()
+
+"""
+## Select Columns of Interests from the Dataset
+"""
+
+options = st.multiselect("Select the columns that you think relevant for estimating the demand", df.columns[2:], ["code_commune_titulaire","pays_titulaire", "marque", "date_premiere_immatriculation", "type_version_variante", "poids_a_vide_national", "categorie_vehicule", "carrosserie_ce", "cylindree", "puissance_net_maxi", "energie", "niv_sonore", "co2", "classe_env"])
+
+@st.cache_data
+def select_vehicle_data(df, options):
+    df_selected = df[options]
+    df_selected['date_premiere_immatriculation'] = pd.to_datetime(df['date_premiere_immatriculation'], errors='coerce', format='%d/%m/%Y').dt.normalize()
+    to_numeric = ['poids_a_vide_national','cylindree','niv_sonore','co2', 'puissance_net_maxi']
+    for col in to_numeric:
+        df_selected[col] = pd.to_numeric(df_selected[col], errors='coerce')
+    return df_selected
+
+df_selected = select_vehicle_data(df, options)
+st.write("Understanding the data:")
+dtypes = df_selected.dtypes
+
+object_cols = dtypes[dtypes == 'object'].index
+numeric_cols = dtypes[dtypes == 'float64'].index
+
+datetime_cols = dtypes[dtypes == 'datetime64[ns]'].index
+
+if st.button("Visualize categorical data"):
+    for col in object_cols:
+        value_counts = df_selected[col].value_counts()
+        if len(value_counts) > 20:
+            value_counts = value_counts.nlargest(20)
+        plt.figure(figsize=(10,5))
+        sns.barplot(x=value_counts.index, y=value_counts.values)
+        plt.xticks(rotation=45)
+        plt.title(f"Distribution of {col}")
+        st.pyplot(plt)
+
+if st.button("Visualize numerical data"):
+    for col in numeric_cols:
+        fig, ax = plt.subplots(figsize=(10,5))
+        sns.boxplot(df_selected[col], ax=ax)
+        ax.set_title(f"Distribution of {col}")
+        ax.tick_params(axis='x', rotation=45) 
+        st.pyplot(plt)
+
+if st.button("Visualize datetime data"):
+    st.write(datetime_cols)
+    for col in datetime_cols:
+        st.subheader(f'Distribution of {col}')
+        # Extract year to make it easier to visualize
+        df_selected['year'] = df_selected[col].dt.year
+        plt.figure(figsize=(10,5))
+        sns.histplot(df_selected['year'], bins=len(df_selected['year'].unique()), kde=False)
+        plt.title(f"Distribution of {col} by Year")
+        st.pyplot(plt)
+
+st.markdown(
+    """
+    ## Geographical Distribution
+    """
+)
+
+map_df = create_map()
+group_commune_df = df_selected.groupby('code_commune_titulaire').count().iloc[:, :1]
+group_commune_df.columns.values[0]="Num Vehicles"
+group_brand_df = df_selected.groupby(['marque', 'type_version_variante', 'code_commune_titulaire']).count().iloc[:, :1]
+group_brand_df.columns.values[0]="Num Vehicles"
+group_brand_df.sort_values(by='Num Vehicles', ascending=False,inplace=True)
+group_brand_df = group_brand_df.reset_index(level=['marque','type_version_variante', 'code_commune_titulaire'])
+
+df_merged = map_df.merge(group_commune_df, left_index=True, right_index=True).sort_values(by='Num Vehicles', ascending=False)    
+
+n = st.number_input("The number of cities to be displayed by default",min_value=1, value="min", step=5)
+geo_filters = st.multiselect(
+    'Select cities to be analysed',
+    df_merged.index,
+    np.array(df_merged.index[:n])
+)
+
+fig = px.bar(group_brand_df[group_brand_df['code_commune_titulaire'].isin(geo_filters)], x='type_version_variante', y='Num Vehicles', hover_data=['code_commune_titulaire', 'Num Vehicles','marque'])
+fig.update_xaxes(range=(-.5,15))
+df_merged = df_merged[df_merged.index.isin(geo_filters)]
+fig_map = px.choropleth_mapbox(df_merged, geojson = df_merged['geometry'], color='Num Vehicles', locations=df_merged.index, mapbox_style="carto-positron", zoom=5.5, center = {"lat": 43.5, "lon": 2}, color_continuous_scale='Greys')
+
+col1, col2 = st.columns([1, 1.5])
+
+with col1:
+    st.plotly_chart(fig)
+with col2:    
+    st.plotly_chart(fig_map)
+
+
+st.markdown(
+    """
+    ## Vehicle Age & Brand
+    """
+)
+current_year = 2024
+df_selected['Age']= current_year - df_selected['date_premiere_immatriculation'].dt.year
+group_age_df = df_selected.groupby(['Age', 'marque', 'type_version_variante']).count().iloc[:, :1]
+group_age_df.columns.values[0] = 'Num Vehicles'
+group_age_df.reset_index(['marque', 'type_version_variante'], inplace=True)
+group_age_df.sort_values('Num Vehicles', inplace=True, ascending=False)
+
+col1, col2, col3 = st.columns([2,2,1])
+
+with col1:
+    brand_filters = st.multiselect(
+        'Select brands to be analysed',
+        group_age_df['marque'].unique(),
+        ['RENAULT', 'VOLKSWAGEN']
+    )
+    if st.checkbox("Select all the brands"):
+        brand_filters = None
+    
+
+with col2:
+    min_age, max_age = st.select_slider(
+                        'Select vehicle age range',
+                        options = np.array([i for i in range(1, 25)]),
+                        value = [1, 15]
+                        )   
+if brand_filters:
+    group_age_df = group_age_df[group_age_df['marque'].isin(brand_filters)]
+group_age_df = group_age_df[(group_age_df.index>min_age) & (group_age_df.index<max_age)]
+# if brand_filters != None:
+#     group_commune_df_filtered = df_selected[df_selected['marque'].isin(brand_filters)]
+# else:
+#     group_commune_df_filtered = df_selected
+# group_commune_df_filtered = group_commune_df_filtered[(df_selected.Age>min_age) & (df_selected.Age<max_age)]
+# group_commune_df_filtered = group_commune_df_filtered.groupby(by = 'code_commune_titulaire').count().iloc[:,:1]
+# group_commune_df_filtered.columns.values[0] = 'Num Vehicles'
+# df_merged = map_df.merge(group_commune_df_filtered, left_index=True, right_index=True).sort_values(by='Num Vehicles', ascending=False)    
+
+with col3:
+    st.metric('Number of Vehicles Selected', group_age_df['Num Vehicles'].sum())
+
+# col1, col2 = st.columns([1,1])
+
+# with col1:
+st.plotly_chart(
+    px.bar(group_age_df, y='Num Vehicles', hover_data=['marque', 'type_version_variante'])
+)
+
+# with col2:
+#     st.plotly_chart(
+#         px.choropleth_mapbox(df_merged, geojson = df_merged['geometry'], color='Num Vehicles', locations=df_merged.index, mapbox_style="carto-positron", zoom=5.5, center = {"lat": 43.5, "lon": 2}, color_continuous_scale='Greys') 
+#     )
+
+st.markdown(
+    """
+    A preview for the dataset Total Market produced:
+    """
+)
+
+st.write(df_selected.head(10))
+
+@st.cache_data
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(df_selected)
+
+st.write("which can be downloaded")
+st.download_button("here", csv, mime='text/csv', file_name='vehicle_list_TM.csv')
+st.write("*N.B.* In this step, the relevant columns of the dataset of existing products have been selected. The previous two sections Geographical Distribution and Vehicle Age & Brand are only for data understanding which won't change the output of this page.")
