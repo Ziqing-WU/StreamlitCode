62d6532e44c7e149c6445cbb81af65f55971e7b7
Ziqing Wu <ziqingwu@outlook.com>
2024-12-05 16:43:26 +0100

    change folder path

--- DemandGenerator/config.py
+++ DemandGenerator/config.py
@@ -37,9 +37,14 @@ from collections import defaultdict
 current_year = 2024
 # The existing product data source
 source_path = r'C:\Users\zwu\Documents\Data\Vehicle\occitanie_cleaned_code_INSEE.csv'
+if not os.path.exists(source_path):
+    source_path = r'\Data\Vehicle\occitanie_cleaned_code_INSEE.csv'
+
 # The folder containing all the intermediate data for each step
 precharged_folder = r'C:\Users\zwu\Documents\StreamlitCode\DemandGenerator\data_precharged'
 executive_factor_folder = r'C:\Users\zwu\Documents\Data\ExecutiveFactors'
+if not os.path.exists(executive_factor_folder):
+    executive_factor_folder = r'\Data\ExecutiveFactors'
 
 def change_type(df_vehicles, to_numeric=['poids_a_vide_national','cylindree','niv_sonore','co2', 'puissance_net_maxi', 'Age']):
     df_vehicles['date_premiere_immatriculation'] = pd.to_datetime(df_vehicles['date_premiere_immatriculation'], errors='coerce', format='%d/%m/%Y').dt.normalize()
